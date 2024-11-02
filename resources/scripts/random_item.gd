d30e89e472443977f57a5005ae8ebcb115be3cfe
elysonia <elysoniaalim@gmail.com>
2024-11-02 16:14:07 +0800

    Format files

--- resources/scripts/random_item.gd
+++ resources/scripts/random_item.gd
@@ -1,6 +1,5 @@
-extends Resource
 class_name RandomItem
-
+extends Resource
 
 @export var name: StringName
 @export var pick_chance: float
--- resources/scripts/random_picker.gd
+++ resources/scripts/random_picker.gd
@@ -1,5 +1,5 @@
-extends Node
 class_name RandomPicker
+extends Node
 # Reference: https://www.youtube.com/watch?v=IAhBZLlz5wY&ab_channel=JohnIvess
 
 @export var item_list: Array[RandomItem] = []
@@ -8,7 +8,7 @@ class_name RandomPicker
 func pick_random_item(items = null):
 	var picked_item = null
 	var overall_pick_chance = 0.0
-	
+
 	var valid_item_list = items if items else item_list
 
 	# Calculate the overall pick chance
@@ -16,11 +16,10 @@ func pick_random_item(items = null):
 		if item.can_be_picked:
 			overall_pick_chance += item.pick_chance
 
-
 	# Generate random number between 0 and 1
 	var random_number = fmod(randf(), overall_pick_chance)
 
-	var offset = 0.0 
+	var offset = 0.0
 	for item in valid_item_list:
 		if item.can_be_picked:
 			if random_number < item.pick_chance + offset:
