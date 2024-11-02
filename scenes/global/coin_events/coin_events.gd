c50beb041f0000f94b7794349d083689c74256ae
elysonia <elysoniaalim@gmail.com>
2024-11-02 21:23:12 +0800

    Add more descriptive comments

--- scenes/global/coin_events/coin_events.gd
+++ scenes/global/coin_events/coin_events.gd
@@ -1,3 +1,4 @@
+# Event bus for communicating coin events to distant nodes.
 class_name CoinEvents
 extends Node2D
 
@@ -5,13 +6,16 @@ extends Node2D
 #	- Maybe it should be moved to coin_flip_hud
 signal coin_timer_text_updated(time: float)
 
+# Emit when a coin side has picked randomly
 # Should:
 # 	- Hide arrow buttons and timer
 # 	- Play animation of the result
 signal coin_random_picker_picked(result_coin)
 
+# Emit when the player picks a coin face
 # Should:
-# 	- Hide coin faces
-# 	- Start the delay timer
-# 	- Show arrow buttons and timer
+# 	- Hide the UI for coin faces
+# 	- Start the delay countdown
+# 	- Show the UI for arrow buttons and delay countdown text
+#	- Show default coin face
 signal coin_player_picked(player_coin)
