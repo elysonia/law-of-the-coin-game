1e2e9eec980bd98d20947a9d3aa966051555be4f
elysonia <elysoniaalim@gmail.com>
2024-11-02 16:34:57 +0800

    Additional text for telling the player the coin will flip after some time

--- scenes/coin_flip/coin_flip_hud/coin_flip_hud.gd
+++ scenes/coin_flip/coin_flip_hud/coin_flip_hud.gd
@@ -1,22 +1,33 @@
 extends Control
 
-
 @onready var _timer_label = $TimerLabel
+@onready var _timer_title_label = $TimerTitleLabel
 
 
 func _ready():
-	_timer_label.hide()
+	_hide_timer_texts()
+
 
 func _process(_delta):
 	pass
 
 
-func _on_flip_delay_timer_timer_label_updated(time:Variant):
+func _show_timer_texts():
+	_timer_label.show()
+	_timer_title_label.show()
+
+
+func _hide_timer_texts():
+	_timer_label.hide()
+	_timer_title_label.hide()
+
+
+func _on_flip_delay_timer_timer_label_updated(time: Variant):
 	_timer_label.text = str(time)
 
 	if _timer_label.hidden:
-		_timer_label.show()
+		_show_timer_texts()
 
 
 func _on_flip_delay_timer_timeout():
-	_timer_label.hide()
+	_hide_timer_texts()
