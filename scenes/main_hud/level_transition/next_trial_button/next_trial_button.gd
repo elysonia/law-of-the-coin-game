c36db27ed56fbb762ef047d8a1cbdc40141967fb
elysonia <elysoniaalim@gmail.com>
2024-11-09 02:11:31 +0800

    Prevent next trial button from showing at the last level

--- scenes/main_hud/level_transition/next_trial_button/next_trial_button.gd
+++ scenes/main_hud/level_transition/next_trial_button/next_trial_button.gd
@@ -10,7 +10,7 @@ func _pressed():
 	var next_level_index = GlobalLevelState.current_level_index + 1
 
 	# Prevent attempting to start a level that doesn't exist
-	if next_level_index > GlobalLevelState._available_levels.levels.size() - 1:
+	if not _check_next_level_exists(next_level_index):
 		return
 
 	var next_level = GlobalLevelState.get_level(next_level_index)
@@ -21,5 +21,17 @@ func _pressed():
 	GlobalLevelState.goto_game_level_scene()
 
 
+func _check_next_level_exists(next_level_index):
+	var last_level_index = GlobalLevelState._available_levels.levels.size() - 1
+
+	return next_level_index < last_level_index
+
+
 func _on_coin_flip_succeeded():
+	var next_level_index = GlobalLevelState.current_level_index + 1
+
+	# Prevent showing the button when player has reached the end
+	if not _check_next_level_exists(next_level_index):
+		return
+
 	show()
