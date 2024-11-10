3f75c9db63e1dcf98a9240bbaf15da368dd43462
elysonia <elysoniaalim@gmail.com>
2024-11-11 02:24:54 +0800

    Create scripts for modifiers

---
+++ resources/scripts/modifier.gd
@@ -0,0 +1,19 @@
+class_name Modifier
+extends RandomItem
+
+@export_enum("ITEM", "PERK") var type: String
+## Conditions for modifier appearance.
+@export_enum("ANYTIME", "NEXT_LEVEL_LAST", "BLURRY_VISION") var appear_condition: String
+## Buying price for modifier.
+@export var price: int
+## Modifier description. Each element is a new line.
+@export var description: PackedStringArray
+
+## Effects activating on the current trial and affects on the current trial only.
+@export var trial_effects: ModifierEffects
+## Effects activating on the next trial and affects the next trial only.
+@export var next_trial_effects: ModifierEffects
+## Effects activating on the current trial and lasts multiple trials.
+@export var multi_trial_effects: ModifierEffects
+## Path to image representation
+@export_file(".png", ".jpg", ".jpeg", ".svg") var image
---
+++ resources/scripts/modifier_effects.gd
@@ -0,0 +1,43 @@
+class_name ModifierEffects
+extends Resource
+
+@export_category("Player choice success rate")
+## Fixed success rate of chosen coin side.
+@export var fixed_coin_pick_chance: float
+## Scale success rate depending on the level. e.g. 0.1 for 10% and 2.0 double the chance each round.
+@export var coin_pick_chance_increment: float
+
+## Specify fixed or range of cost for next trial.
+@export_category("Trial cost")
+## Fixed cost of next trial.
+@export var fixed_trial_cost: int
+@export var fixed_trial_cost_desc: String
+## Additional range of cost for next trial.
+@export_range(0, 10) var range_trial_cost
+@export var range_trial_cost_desc: String
+
+## Additional fun effects.
+@export_category("Special Effects")
+## Increase button mashing time in seconds
+@export var button_mash_time_increase: int
+## Disadvantages for the current, next or subsequent trials.
+@export_enum(
+	"NONE", "NO_ITEMS", "NO_MONEY", "NO_PERKS", "BLURRY_VISION", "LOWER_MODIFIER_EFFECTIVENESS"
+)
+var handicap: String
+## For LOWER_MODIFIER_EFFECTIVENESS (0.0 - 1.0)
+@export var decrease_modifier_effectiveness_by: float
+
+@export_category("Effect State")
+## Change to true while in use
+var _is_in_effect: bool = false
+## Change to true after use
+var _was_in_effect: bool = false
+
+
+func set_is_in_effect(value: bool):
+	_is_in_effect = value
+
+
+func set_was_in_effect(value: bool):
+	_was_in_effect = value
