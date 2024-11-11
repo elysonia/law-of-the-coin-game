5cf3fb1930ca73e825674569d921f189aeb83a0a
elysonia <elysoniaalim@gmail.com>
2024-11-12 00:14:11 +0800

    Fix unclear property hints

--- resources/scripts/modifier_effects.gd
+++ resources/scripts/modifier_effects.gd
@@ -3,6 +3,8 @@ extends Resource
 
 @export_category("Player choice success rate")
 ## Fixed success rate of chosen coin side.
+## Replaces the default level coin pick chance.
+## Stacks with coin_pick_chance_increment.
 @export var fixed_coin_pick_chance: float
 ## Scale success rate depending on the level. e.g. 0.1 for 10% and 2.0 double the chance each round.
 @export var coin_pick_chance_increment: float
@@ -11,9 +13,11 @@ extends Resource
 @export_category("Trial cost")
 ## Fixed cost of the trial.
 @export var fixed_trial_cost: int
+## Description of trial cost for in-game hints.
 @export var fixed_trial_cost_desc: String
 ## Additional range of cost for the trial.
 @export_range(0, 10) var range_trial_cost
+## Description of range of trial cost for in-game hints.
 @export var range_trial_cost_desc: String
 
 ## Additional fun effects.
@@ -25,19 +29,19 @@ extends Resource
 	"NONE", "NO_ITEMS", "NO_MONEY", "NO_PERKS", "BLURRY_VISION", "LOWER_MODIFIER_EFFECTIVENESS"
 )
 var handicap: String
-## For LOWER_MODIFIER_EFFECTIVENESS (0.0 - 1.0)
-@export var decrease_modifier_effectiveness_by: float
+## For when the modifier counters other level modifiers.
+@export var decrease_level_modifiers_effectiveness_by: float
 
 @export_category("Effect State")
 ## Change to true while in use
-var _is_in_effect: bool = false
+var _is_in_use: bool = false
 ## Change to true after use
-var _was_in_effect: bool = false
+var _is_used: bool = false
 
 
-func set_is_in_effect(value: bool):
-	_is_in_effect = value
+func set_is_in_use(value: bool):
+	_is_in_use = value
 
 
-func set_was_in_effect(value: bool):
-	_was_in_effect = value
+func set_is_used(value: bool):
+	_is_used = value
