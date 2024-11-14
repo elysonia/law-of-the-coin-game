cc193e5b57e3f54e76a21cda9dec0eca2fb7dbd3
elysonia <elysoniaalim@gmail.com>
2024-11-14 19:34:27 +0800

    Reset default increment rate

--- scenes/global/enums.gd
+++ scenes/global/enums.gd
@@ -5,5 +5,5 @@ extends Node
 # INDEX is the neutral value for when no choices have been made
 const COIN = {INDEX = "index", HEADS = "heads", TAILS = "tails"}
 
-const ARROW_KEY_INCREMENT_RATE = 0.1
+const ARROW_KEY_INCREMENT_RATE = 0.01
 const DEFAULT_REWARD_MONEY = 10
