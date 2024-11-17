71083d7fa25e8172c21f0e87200903de4c8c9cdd
elysonia <elysoniaalim@gmail.com>
2024-11-18 01:18:19 +0800

    Remove unused file

--- scenes/game_level/game_level.gd
+++
@@ -1,9 +0,0 @@
-class_name GameLevel
-extends Node2D
-
-@onready var _level_label = $LevelLabel
-
-
-func _ready():
-	var level_number = GlobalLevelState.current_level_index + 1
-	_level_label.text = "Trial " + str(level_number)
