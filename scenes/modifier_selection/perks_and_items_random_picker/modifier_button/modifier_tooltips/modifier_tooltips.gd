742fa99ba2c10dae75561a407bb5c44dc4fd8a19
elysonia <elysoniaalim@gmail.com>
2024-11-21 17:56:33 +0800

    Add price to the tooltips

--- scenes/modifier_selection/perks_and_items_random_picker/modifier_button/modifier_tooltips/modifier_tooltips.gd
+++ scenes/modifier_selection/perks_and_items_random_picker/modifier_button/modifier_tooltips/modifier_tooltips.gd
@@ -9,13 +9,23 @@ func initialize(modifier: Modifier):
 		+ ": "
 		+ "[color=orange]"
 		+ modifier.display_name
-		+ "[/color][/b][/font_size]\n\n"
+		+ "[/color][/b][/font_size]\n"
 	)
+
+	var price = (
+		"[font_size={15}][b]"
+		+ str(modifier.price)
+		+ " coins"
+		+ "[/b][/font_size]\n\n"
+	)
+
 	var flavor_text = "[color=yellow][i]" + modifier.description[0] + "[/i][/color]\n\n"
 	var buff_text = "[color=green]" + modifier.description[1] + "[/color]\n\n"
 	var debuff_text = "[color=red]" + modifier.description[2] + "[/color]\n"
 	visible = true
+
 	append_text(title)
+	append_text(price)
 	append_text(flavor_text)
 	append_text(buff_text)
 	append_text(debuff_text)
