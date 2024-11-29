fe36c97f5fcda1d2f2cb70cb3d6a135996656736
elysonia <elysoniaalim@gmail.com>
2024-11-29 22:26:44 +0800

    Custom additional pick chance calculation for perk

--- resources/modifiers/perks/perk_photogenic.gd
+++ resources/modifiers/perks/perk_photogenic.gd
@@ -1,12 +1,22 @@
 extends ModifierManager
 
 
+func _get_additional_coin_pick_chance(effects):
+	if effects.coin_pick_chance_increment == -2.0:
+		return (
+			_get_exponential_value_for_current_level(effects.fixed_coin_pick_chance) / 2
+		)
+
+	if effects.coin_pick_chance_increment == 2.0:
+		return _get_exponential_value_for_current_level(effects.fixed_coin_pick_chance)
+
+	return effects.coin_pick_chance_increment * (GlobalLevelState.current_level_index)
+
+
 func _change_player_choice_success_rate(effects):
 	# Reduce the success rate at the rate of success in the previous round.
 	if effects.coin_pick_chance_increment == -2.0:
-		var current_increment_rate = (
-			_get_exponential_value_for_current_level(effects.fixed_coin_pick_chance) / 2
-		)
+		var current_increment_rate = _get_additional_coin_pick_chance(effects)
 		GlobalLevelState.player_win_rate += current_increment_rate
 		return
 
