7bae238765f0278f1f5bca7576905ec1d1c118ed
elysonia <elysoniaalim@gmail.com>
2024-11-29 22:27:19 +0800

    Fix blur filter init/cleanup

--- resources/modifiers/perks/perk_self_study.gd
+++ resources/modifiers/perks/perk_self_study.gd
@@ -5,9 +5,13 @@ var _instantiated_simple_blur = null
 
 
 func goto_next_trial():
-	_instantiated_simple_blur = _simple_blur_filter.instantiate()
+	if is_instance_valid(_instantiated_simple_blur):
+		_instantiated_simple_blur.queue_free()
+		_instantiated_simple_blur = null
+
 
 	if _modifier.trial_effects != null:
+		_instantiated_simple_blur = _simple_blur_filter.instantiate()
 		GlobalLevelState.level_modifier_handicaps.append(_modifier.trial_effects.handicap)
 		_parent_node.add_child(
 			_instantiated_simple_blur
@@ -17,22 +21,28 @@ func goto_next_trial():
 		_modifier.multi_trial_effects != null
 		and _modifier.multi_trial_effects.handicap not in GlobalLevelState.level_modifier_handicaps
 	):
+		_instantiated_simple_blur = _simple_blur_filter.instantiate()
 		GlobalLevelState.level_modifier_handicaps.append(_modifier.multi_trial_effects.handicap)
 		_parent_node.add_child(
 			_instantiated_simple_blur
 		)
 
 
-func start_trial():
-	super.start_trial()
+func start_multi_trial_effects():
+	super.start_multi_trial_effects()
+
+	if is_instance_valid(_instantiated_simple_blur):
+		_instantiated_simple_blur.queue_free()
+		_instantiated_simple_blur = null
+
 	_instantiated_simple_blur = _simple_blur_filter.instantiate()
 	_parent_node.add_child(
 		_instantiated_simple_blur
 	)
 
 
-func stop_current_trial_effects():
-	super.stop_current_trial_effects()
+func stop_multi_trial_effects():
+	super.stop_multi_trial_effects()
 
 	if is_instance_valid(_instantiated_simple_blur):
 		_instantiated_simple_blur.queue_free()
