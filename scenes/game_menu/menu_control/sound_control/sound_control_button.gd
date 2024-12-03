0e8bc5588065514daa59a3beb51c95b0ff764094
elysonia <elysoniaalim@gmail.com>
2024-12-03 11:14:19 +0800

    Fix volume container due to not taking account the global position
    Clicking inside the bounds of the volume slider container hides the container when not.
    
    Also changed from instantiating node on demand to regular hiding and showing, it has no relation to the problem, just possible to do because theres nothing below the volume slider that needs clicking

--- scenes/game_menu/menu_control/sound_control/sound_control_button.gd
+++ scenes/game_menu/menu_control/sound_control/sound_control_button.gd
@@ -1,40 +1,35 @@
 extends TextureButton
 
-var _volume_slider_container_instance = null
 
-@onready var _volume_slider_container = preload(
-	"res://scenes/game_menu/menu_control/sound_control/volume_slider_container.tscn"
-)
+@onready var _volume_slider_container = $VolumeSliderContainer
+
+
+func _ready():
+	_volume_slider_container.hide()
 
 
 func _input(event):
-	if not is_instance_valid(_volume_slider_container_instance):
-		return
-
 	if not is_instance_of(event, InputEventMouseButton):
 		return
 
 	var mouse_position = event.get_position()
 	var volume_slider_boundary = Rect2(
-		Vector2(_volume_slider_container_instance.position), _volume_slider_container_instance.size
+		_volume_slider_container.global_position, _volume_slider_container.size
 	)
-	var sound_button_boundary = Rect2(position, size)
+	var sound_button_boundary = Rect2(global_position, size)
 
 	var is_clicked_outside_volume_control = (
 		not volume_slider_boundary.has_point(mouse_position)
 		and not sound_button_boundary.has_point(mouse_position)
 	)
-	print({"is_clicked_outside_volume_control": is_clicked_outside_volume_control})
+
 	if is_clicked_outside_volume_control:
-		_volume_slider_container_instance.queue_free()
-		_volume_slider_container_instance = null
+		_volume_slider_container.hide()
 
 
 func _pressed():
-	if is_instance_valid(_volume_slider_container_instance):
-		_volume_slider_container_instance.queue_free()
-		_volume_slider_container_instance = null
+	if _volume_slider_container.visible:
+		_volume_slider_container.hide()
 		return
 
-	_volume_slider_container_instance = _volume_slider_container.instantiate()
-	add_child(_volume_slider_container_instance)
+	_volume_slider_container.show()
