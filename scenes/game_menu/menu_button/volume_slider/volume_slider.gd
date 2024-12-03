cd2123c3e64c4ba2d95ae7dcb445d4da308b6ffc
elysonia <elysoniaalim@gmail.com>
2024-12-04 00:22:12 +0800

    Fix audio cracking at higher volume and ensure sounds can be muted
    By narrowing the range of the slider and pause all sounds manually at min_value

--- scenes/game_menu/menu_button/volume_slider/volume_slider.gd
+++ scenes/game_menu/menu_button/volume_slider/volume_slider.gd
@@ -6,6 +6,11 @@ func _ready():
 
 
 func _value_changed(new_value):
+	if new_value == min_value:
+		SoundManager.pause_all()
+		return
+
+	SoundManager.unpause_all()
 	SoundManager.set_bgm_volume_db(new_value)
 	SoundManager.set_bgs_volume_db(new_value)
 	SoundManager.set_sfx_volume_db(new_value)
